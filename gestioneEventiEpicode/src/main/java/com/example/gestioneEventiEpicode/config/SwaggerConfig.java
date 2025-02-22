e286207be10714fad70dddef74a8e65b273f1e2a
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-02-22 14:47:00 +0100

    SwaggerConfig+utilizzo annotazione swagger

---
+++ gestioneEventiEpicode/src/main/java/com/example/gestioneEventiEpicode/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.gestioneEventiEpicode.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080").description("Server di sviluppo"))
+                .info(new Info()
+                        .title("API dell'Applicazione Eventi")
+                        .version("1.0")
+                        .description("Documentazione delle API per la gestione degli eventi"))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .name("JWT")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
--- gestioneEventiEpicode/src/main/java/com/example/gestioneEventiEpicode/controller/EventoController.java
+++ gestioneEventiEpicode/src/main/java/com/example/gestioneEventiEpicode/controller/EventoController.java
@@ -3,6 +3,8 @@ package com.example.gestioneEventiEpicode.controller;
 import com.example.gestioneEventiEpicode.entity.Evento;
 import com.example.gestioneEventiEpicode.entity.Utente;
 import com.example.gestioneEventiEpicode.service.EventoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,6 +21,8 @@ public class EventoController {
     private EventoService eventoService;
 
     @PostMapping
+    @Operation(summary = "Crea un nuovo evento", description = "Crea un nuovo evento con i dettagli forniti")
+    @ApiResponse(responseCode = "201", description = "Evento creato con successo")
     public ResponseEntity<Evento> creaEvento(@RequestBody Evento evento, @AuthenticationPrincipal Utente organizzatore) {
         Evento nuovoEvento = eventoService.creaEvento(evento, organizzatore);
         return ResponseEntity.ok(nuovoEvento);
